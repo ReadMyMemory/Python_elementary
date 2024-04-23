5eb8bde7835b08ab72524b74c9c30743215e07fe
ReadMyMemory <lame3925@gmail.com>
2024-04-24 02:20:06 +0900

    chapter5 ??

---
+++ Mid_term_exam_prepare/Chapter5/exercise.py
@@ -0,0 +1,20 @@
+'''
+#1.
+equal이라는 건 없고, equals는 있다.
+== 과 is의 차이점은
+is는 객체 간 값 비교고
+== 은 값간 동일 여부 비교다.
+
+#5.삼항연산자
+res = 첫번째 값 조건문 else 두번째값 하면 됨.
+res = '짝수' if num % 2 == 0 else '홀수'
+
+#6.랜덤하게 숫자 20개 채우기
+list = []
+list에 값 넣는 건 append
+
+
+for _ in range(20):
+    random_number = random.randint(1, 20)
+    random_numbers.append(random_number)
+'''
